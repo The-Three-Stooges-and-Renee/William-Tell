696620ca0c8e48e19cbde569b68f1a5bcf1b0801
thegeekkid <brian@geekkidconsulting.com>
2016-03-28 19:11:58 -0500

    Trying to format date as mdy instead of ymd.

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     {
       header('Location: http://submit-iit-timesheet.geekkidconsulting.com/login.aspx');
     }
-  $dat = date("Y-m-d");
+  $dat = date("m-d-Y");
   if (!($_GET['date'] == ''))
   {
     $dat = $_GET['date'];
