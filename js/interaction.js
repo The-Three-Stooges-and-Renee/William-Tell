d9d91bef3655a6e44702593ce38c4f183fbb68b9
thegeekkid <brian@geekkidconsulting.com>
2016-03-29 21:16:07 -0500

    First try of ajax

--- js/interaction.js
+++ js/interaction.js
@@ -1,6 +1,12 @@
 $( document ).ready(function() {
   $( '.jshide' ).addClass('hidden').removeClass('jshide');
-  $( "#date" ).blur(function() {
-    window.location.replace('index.php?date=' + $('#date').val());
+  $( "#date" ).onchange(function() {
+    //window.location.replace('index.php?date=' + $('#date').val());
+    $.ajax({
+      url: ('http://submit-iit-timesheet.geekkidconsulting.com/default.aspx?action=pull&date=' + $('#date').val())
+    }).done(setinfo();)
   })
 });
+function setinfo(){
+  alert('setting info');
+}
