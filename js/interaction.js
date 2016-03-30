7d40fcbb408d616e705c0bb68a2f2fcd3ac5b18d
thegeekkid <brian@geekkidconsulting.com>
2016-03-29 20:59:48 -0500

    First attempt at redirect on date change with JQuery.

--- js/interaction.js
+++ js/interaction.js
@@ -1,4 +1,6 @@
 $( document ).ready(function() {
   $( '.jshide' ).addClass('hidden').removeClass('jshide');
-  
+  $( "#date" ).focusout(function() {
+    window.location.replace('index.php?date=' + $('#date').val());
+  })
 });
