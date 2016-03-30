a9776b6bc9cce08bb3b7425c7fedf0584020e4ae
thegeekkid <brian@geekkidconsulting.com>
2016-03-29 21:26:28 -0500

    Trying a input bind instead of change event.

--- js/interaction.js
+++ js/interaction.js
@@ -1,9 +1,10 @@
 $( document ).ready(function() {
   $( '.jshide' ).addClass('hidden').removeClass('jshide');
-  $( "#date" ).change(function() {
+  $( '#date' ).bind('input', function(){
     //window.location.replace('index.php?date=' + $('#date').val());
     $.ajax({
-      url: ('http://submit-iit-timesheet.geekkidconsulting.com/default.aspx?action=pull&date=' + $('#date').val())
+      url: ('http://submit-iit-timesheet.geekkidconsulting.com/default.aspx?action=pull&date=' + $('#date').val()),
+      crossDomain: true
     }).done(setinfo());
   })
 });
