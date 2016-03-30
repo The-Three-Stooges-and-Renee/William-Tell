79c34a65ec1108d98dc1a78ae81ceea64a13582e
thegeekkid <brian@geekkidconsulting.com>
2016-03-29 21:07:13 -0500

    Changed focusout to blur

--- js/interaction.js
+++ js/interaction.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
   $( '.jshide' ).addClass('hidden').removeClass('jshide');
-  $( "#date" ).focusout(function() {
+  $( "#date" ).blur(function() {
     window.location.replace('index.php?date=' + $('#date').val());
   })
 });
