535727bf23e9625a9fc0d49ae9a973c90df67adf
thegeekkid <brian@geekkidconsulting.com>
2016-04-04 16:26:57 -0500

    Started rounding with JS

--- js/interaction.js
+++ js/interaction.js
@@ -226,6 +226,13 @@ function evaluateshown(speed) {
     hideline('#out3', speed);
     hideline('#in4', speed);
     hideline('#out4', speed);
+    if (!(($('#inm1').val() == 00) || ($('#inm1').val() == 0))) {
+      if (!(round15($('#inm1')) == 60)) {
+        $('#inm1').val(round15($('#inm1')));
+      }else {
+        
+      }
+    }
   }else {
     showline('#out1', speed);
   }
@@ -277,4 +284,43 @@ function evaluateshown(speed) {
   }else {
     showline('#out4', speed);
   }
+}
+function round15(inpt) {
+  var rounded = inpt;
+  if (inpt < 15) {
+    if (inpt < 7) {
+      rounded = 00;
+    }else {
+      rounded = 15;
+    }
+  }else {
+    if (inpt < 30) {
+      if (inpt < 22) {
+        rounded = 15;
+      }else {
+        rounded = 30;
+      }
+    }else {
+      if (inpt < 45) {
+        if (inpt < 37) {
+          rounded = 30;
+        }else {
+          rounded = 45;
+        }
+        else {
+          if (inpt < 60) {
+            if (inpt < 52) {
+              rounded = 45;
+            }else {
+              rounded = 60;
+            }
+          }else {
+            alert("Error: Didn't understand the minutes entry.")
+            return false;
+          }
+        }
+      }
+    }
+  }
+  return rounded;
 }
