b86690256cc9e7c58621aa4b48bc1670e7fe6a94
thegeekkid <brian@geekkidconsulting.com>
2016-04-04 16:52:42 -0500

    Uncomment out JS for debugging.

--- js/interaction.js
+++ js/interaction.js
@@ -227,11 +227,11 @@ function evaluateshown(speed) {
     hideline('#in4', speed);
     hideline('#out4', speed);
      if (!(($('#inm1').val() == 00) || ($('#inm1').val() == 0))) {
-      /*if (!(round15($('#inm1')) == 60)) {
+      if (!(round15($('#inm1')) == 60)) {
         $('#inm1').val(round15($('#inm1')));
       }else {
         
-      }*/
+      }
     }
   }else {
     showline('#out1', speed);
@@ -285,7 +285,7 @@ function evaluateshown(speed) {
     showline('#out4', speed);
   }
 }
-/*function round15(inpt) {
+function round15(inpt) {
   var rounded = inpt;
   if (inpt < 15) {
     if (inpt < 7) {
@@ -323,4 +323,4 @@ function evaluateshown(speed) {
     }
   }
   return rounded;
-}*/
+}
