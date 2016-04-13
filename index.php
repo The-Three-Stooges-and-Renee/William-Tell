5a49fe143de13c7deda2c8b46083be04376d44d4
ArturHawkwing1 <ArturHawkwing1@gmail.com>
2016-04-13 17:26:51 -0500

    Added title.

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="end">
 
 	<head>
-		<title></title>
+		<title>IIT Student Welcome</title>
 		<meta charset="utf-8" />
 		<meta name="viewport"
 			  content="width=device-width,initial-scale=1.0",shink-to-fit=no"/>
