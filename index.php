061cb08603f3e8f35f52e6909e4caf4864bd0d2a
ArturHawkwing1 <ArturHawkwing1@gmail.com>
2016-04-13 14:52:02 -0500

    removed class from body tag in index.php.

--- index.php
+++ index.php
@@ -70,7 +70,7 @@
     <!--Link to CSS style sheets.-->
     <link rel="stylesheet" type="text/css" href="css/Screen.css"/>
   </head>
-  <body class="Login">
+  <body>
     <div id="iit_banner">
       <h1>IIT Timesheet Project - you have successfully authenticated!</h1>
     </div> <!-- end of iit_banner -->
