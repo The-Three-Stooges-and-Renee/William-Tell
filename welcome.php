349b5d77ef912dc0eff6c8c4a6403903750754c2
ArturHawkwing1 <ArturHawkwing1@gmail.com>
2016-04-13 14:03:10 -0500

    added welcome.php file with basic HTML5.

---
+++ welcome.php
@@ -0,0 +1,17 @@
+<!Doctype HTML>
+<html lang="end">
+
+	<head>
+		<title></title>
+		<meta charset="utf-8" />
+		<meta name="viewport"
+			  content="width=device-width,initial-scale=1.0",shink-to-fit=no"/>
+		<!--Link to CSS style sheets.-->
+		<link rel="stylesheet"
+			  type="text/css"
+			  href="Screen.css"/>
+	</head>
+	<body>
+		
+	</body>
+</html>
