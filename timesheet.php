7299bcf0045ecec957fb933d23105bf7b5a6808f
ArturHawkwing1 <ArturHawkwing1@gmail.com>
2016-04-15 00:10:21 -0500

    added timesheet class to body tag in timesheet.php.

--- timesheet.php
+++ timesheet.php
@@ -70,7 +70,7 @@
     <!--Link to CSS style sheets.-->
     <link rel="stylesheet" type="text/css" href="css/Screen.css"/>
   </head>
-  <body>
+  <body class="timesheet">
     <div id="iit_banner">
       <button name="signout" id="signout">Sign out</button>
       <h1>IIT Timesheet Project - you have successfully authenticated!</h1>
