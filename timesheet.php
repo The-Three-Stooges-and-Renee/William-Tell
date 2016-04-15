e2a9e27de31eed368a87064bc682cc8a23017968
ArturHawkwing1 <ArturHawkwing1@gmail.com>
2016-04-15 00:42:16 -0500

    deleted the extra Select a date since last commit was CTRL C not CTRL X.

--- timesheet.php
+++ timesheet.php
@@ -87,7 +87,6 @@
     </div> <!-- end of nav_bar -->
 
     <div id="ts-form">
-      <p>Select a date:</p>
       <form action="http://submit-iit-timesheet.geekkidconsulting.com/default.aspx" method="get">
 				<p>Select a date:</p>
         <input type="date" id="date" name="date" value="<?php echo $dat;?>" />
