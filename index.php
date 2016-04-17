ef1a5ea72c17907bc2b63e452f7f6e75d1b151ad
Renee Weaver <weaver@iit.edu>
2016-04-17 15:39:34 -0500

    added public safety info

--- index.php
+++ index.php
@@ -23,11 +23,30 @@
 				<li><a href="#">Sign Out</a></li>
       </ul>
     </div> <!-- end of nav_bar -->
-    <div>
+    <div id="welcome">
       <h2>Welcome to the Illinois Tech Portal</h2>
       <a href="http://web.iit.edu/university-calendar">Happenings</a>
       <a href="https://iit.edu/news/iittoday/">News</a>
       <a href="http://phonebook.iit.edu/">University Phonebook</a>
     </div><!--End of Welcome-->
+    <div id="public-safety">
+      <h2>Public Safety</h2>
+      <h3>Emergency Numbers</h3>
+      <h4>Main Campus</h4>
+      <p>Emergency: 312.808.6363</p>
+      <p>Non-Emergency: 312.808.6300</p>
+      <h4>Downtown Campus</h4>
+      <p>312.906.5030</p>
+      <h4>Moffett Campus</h4>
+      <p>708.563.8280</p>
+      <h4>Rice Campus</h4>
+      <p>630.682.6054</p>
+      <h3>Important Public Safety Links</h3>
+      <a href="http://web.iit.edu/public-safety">Public Safety</a>
+      <a href="http://web.iit.edu/emergency">Emergency Information</a>
+      <a href="https://iit.regroup.com/">IIT Alert</a>
+      <a href="http://blogs.iit.edu/public_safety/">Incident Reports</a>
+      <a href="https://www.facebook.com/IITPublicSafety">Public Safety Facebook Page</a>
+    </div><!--End of Public Safety-->
 	</body>
 </html>
