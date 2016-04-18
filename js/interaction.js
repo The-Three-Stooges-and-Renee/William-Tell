a728aa49f86e486f41a72f5001623bbf396aab50
thegeekkid <brian@geekkidconsulting.com>
2016-04-18 17:23:36 -0500

    add code to auto-select text

--- js/interaction.js
+++ js/interaction.js
@@ -1,5 +1,6 @@
 $( document ).ready(function() {
   //$( '.jshide' ).addClass('hidden').removeClass('jshide');
+  $("input:text").focus(function() { $(this).select(); } );
   hideline('.jshide', 0);
   showline('.jsshow', 0);
   evaluateshown(0);
