0c7565c4dbfb86c3d13419b168a9a571e421ae17
thegeekkid <brian@geekkidconsulting.com>
2016-04-18 21:39:34 -0500

    Added missing space in link to css to adhear to xhtml compatibility.

--- index.php
+++ index.php
@@ -64,7 +64,7 @@
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, shink-to-fit=no" />
 		<!--Link to CSS style sheets.-->
-		<link rel="stylesheet" type="text/css" href="css/Screen.css"/>
+		<link rel="stylesheet" type="text/css" href="css/Screen.css" />
 	</head>
 	<body class="welcome">
 
